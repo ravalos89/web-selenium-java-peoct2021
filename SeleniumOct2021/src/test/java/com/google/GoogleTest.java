24b2a27746e9fc00d34ea9d2d18fdffe37318d90
ricardoavalos <ricardoavalos@localhost>
2021-10-28 20:45:36 -0500

    First commit

---
+++ SeleniumOct2021/src/test/java/com/google/GoogleTest.java
@@ -0,0 +1,21 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+	public static WebDriver driver;
+
+	@Test
+	public void f() {
+
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver");
+		driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+
+	}
+}
